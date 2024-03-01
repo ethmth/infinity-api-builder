6b710e183b43dd374ef256aca4c0ad1ce9b842be
ethmth <ethan@smaze.net>
2024-02-29 21:34:40 -0500

    Updated to 7.0.1

--- src/run.sh
+++ src/run.sh
@@ -18,7 +18,7 @@ else
   #wget --output-file=Infinity.zip "https://github.com/Docile-Alligator/Infinity-For-Reddit/archive/refs/tags/v7.0.0.zip" 
   #unzip "Infinity.zip"
   #mv -T Infinity-For-Reddit-* Infinity-For-Reddit
-  git clone --depth 1 --branch v7.0.0 https://github.com/Docile-Alligator/Infinity-For-Reddit
+  git clone --depth 1 --branch v7.0.1 https://github.com/Docile-Alligator/Infinity-For-Reddit
 fi
 
 wget -P /content/ "https://github.com/TanukiAI/Infinity-keystore/raw/main/Infinity.jks"
